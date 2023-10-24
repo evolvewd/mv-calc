7fa611dca399a2e6d630cf75e6f6f5c05d620872
evolvewd <vincenzo.bevivino@gmail.com>
2023-10-24 23:17:35 +0200

    upload2

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
+document.getElementById("generatePanelSelectors").addEventListener("click", generatePanelSelectors);
 document.getElementById("calculateSize").addEventListener("click", calculateSize);
 
+function generatePanelSelectors() {
+  const panelCount = parseInt(document.getElementById("panel-count").value);
+  const panelTypeSelectors = document.getElementById("panel-type-selectors");
+
+  panelTypeSelectors.innerHTML = ""; // Cancella eventuali selezioni precedenti
+
+  for (let i = 1; i <= panelCount; i++) {
+    const panelSelector = document.createElement("div");
+    panelSelector.innerHTML = `
+      <label for="tipo-${i}" class="block text-sm font-medium">Pannello ${i}:</label>
+      <select id="tipo-${i}" class="w-full py-2 px-3 mb-4 border rounded bg-gray-900 text-white">
+        <option value="elegance">Elegance</option>
+        <option value="pro">Pro</option>
+      </select>
+      <label for="taglia-${i}" class="block text-sm font-medium">Formato ${i}:</label>
+      <select id="taglia-${i}" class="w-full py-2 px-3 mb-4  border rounded bg-gray-900 text-white">
+        <option value="A4">A4</option>
+        <option value="A3">A3</option>
+      </select>
+      <label for="orientamento-${i}" class="block text-sm font-medium">Orientamento del Pannello ${i}:</label>
+      <select id="orientamento-${i}" class="w-full py-2 px-3 mb-4 border rounded bg-gray-900 text-white">
+        <option value="h">Orizzontale (h)</option>
+        <option value="v">Verticale (v)</option>
+      </select>
+    `;
+    panelTypeSelectors.appendChild(panelSelector);
+  }
+}
+
 function calculateSize() {
-  const panelType = document.getElementById("panel-type").value;
-  const paperSize = document.getElementById("paper-size").value;
-  const glassSize = parseInt(document.getElementById("glass-size").value); // Ottieni la misura del vetro e convertila in un numero
-  const panelOrientation = document.getElementById("panel-orientation").value; // Ottieni l'orientamento selezionato
-  const panelCount = parseInt(document.getElementById("panel-count").value); // Ottieni il numero di pannelli
+  const panelCount = parseInt(document.getElementById("panel-count").value);
+  const tipo = document.querySelectorAll('[id^="tipo-"]');
+  const taglia = document.querySelectorAll('[id^="taglia-"]');
+  const orientamento = document.querySelectorAll('[id^="orientamento-"]');
+  const glassSize = parseInt(document.getElementById("glass-size").value);
 
   const json = {
     elegance: {
@@ -14,41 +44,52 @@ function calculateSize() {
           l: 369,
         },
         v: {
-          l: 297,
+          l: 21,
         },
       },
       A3: {
         h: {
-          l: 297,
+          l: 300,
         },
         v: {
-          l: 420,
+          l: 369,
         },
       },
     },
     pro: {
       A4: {
         h: {
-          l: 210,
+          l: 150,
         },
         v: {
-          l: 297,
+          l: 50,
         },
       },
       A3: {
         h: {
-          l: 297,
+          l: 250,
         },
         v: {
-          l: 420,
+          l: 150,
         },
       },
     },
   };
 
-  const panelSize = json[panelType][paperSize][panelOrientation].l; // Ottieni il valore "l" corrispondente all'orientamento del pannello
+  let totalPanelSize = 0;
 
-  const totalPanelSize = panelSize * panelCount; // Calcola la dimensione totale dei pannelli
+  for (let i = 0; i < panelCount; i++) {
+    const selectedPanelType = tipo[i].value;
+    console.log(selectedPanelType);
+    const selectedPaperSize = taglia[i].value;
+    console.log(selectedPaperSize);
+    const selectedPanelOrientation = orientamento[i].value;
+    console.log(selectedPanelOrientation);
+
+    totalPanelSize += json[selectedPanelType][selectedPaperSize][selectedPanelOrientation].l;
+    console.log(totalPanelSize);
+  }
+
   const totalGlassSize = glassSize - totalPanelSize;
   const gap = Math.round(totalGlassSize / (panelCount + 1));
   const centroVetro = glassSize / 2;
