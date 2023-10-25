00dd73e85ef313b485c75b0d1f8279052618a528
evolvewd <vincenzo.bevivino@gmail.com>
2023-10-25 23:10:08 +0200

    upload5

--- script.js
+++ script.js
@@ -10,21 +10,32 @@ function generatePanelSelectors() {
   for (let i = 1; i <= panelCount; i++) {
     const panelSelector = document.createElement("div");
     panelSelector.innerHTML = `
-      <label for="tipo-${i}" class="block text-sm font-medium text-white text-3xl">Pannello ${i}:</label>
-      <select id="tipo-${i}" class="w-full py-2 px-3 mb-4 border rounded bg-gray-900 text-white text-3xl">
+
+
+  
+    <div class="relative my-4">
+      <label for="tipo-${i}" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Pannello ${i}:</label>
+      <select id="tipo-${i}" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600">
         <option value="elegance">Elegance</option>
         <option value="pro">Pro</option>
       </select>
-      <label for="taglia-${i}" class="block text-sm font-medium text-white text-3xl">Formato ${i}:</label>
-      <select id="taglia-${i}" class="w-full py-2 px-3 mb-4  border rounded bg-gray-900 text-white text-3xl">
+    </div>
+    <div class="relative my-4">
+      <label for="taglia-${i}" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Formato ${i}:</label>
+      <select id="taglia-${i}" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600">
         <option value="A4">A4</option>
         <option value="A3">A3</option>
       </select>
-      <label for="orientamento-${i}" class="block text-sm font-medium text-white text-3xl">Orientamento del Pannello ${i}:</label>
-      <select id="orientamento-${i}" class="w-full py-2 px-3 mb-4 border rounded bg-gray-900 text-white text-3xl">
-        <option value="h">Orizzontale (h)</option>
-        <option value="v">Verticale (v)</option>
+    </div>
+    <div class="relative my-4">
+      <label for="orientamento-${i}" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Orientamento ${i}:</label>
+      <select id="orientamento-${i}" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600">
+        <option value="h">Orizzontale</option>
+        <option value="v">Verticale</option>
       </select>
+    </div>
+
+
     `;
     panelTypeSelectors.appendChild(panelSelector);
   }
@@ -35,7 +46,7 @@ function calculateSize() {
   const tipo = document.querySelectorAll('[id^="tipo-"]');
   const taglia = document.querySelectorAll('[id^="taglia-"]');
   const orientamento = document.querySelectorAll('[id^="orientamento-"]');
-  const glassSize = parseInt(document.getElementById("glass-size").value);
+  const glassSize = parseInt(document.getElementById("glass-size").value) * 10;
 
   const json = {
     elegance: {
@@ -90,15 +101,17 @@ function calculateSize() {
     console.log(totalPanelSize);
   }
 
-  const totalGlassSize = glassSize - totalPanelSize;
-  const gap = Math.round(totalGlassSize / (panelCount + 1));
-  const centroVetro = glassSize / 2;
+  const decimale = 1;
+  const totalGlassSize = (glassSize - totalPanelSize) / 10;
+  const gap = totalGlassSize / (panelCount + 1) / 10;
+  const centroVetro = glassSize / 20;
+  const gapArr = gap.toFixed(decimale);
 
-  document.getElementById("meta").innerHTML = `Centro Vetrina: ${centroVetro} mm`;
+  document.getElementById("meta").innerHTML = `Centro Vetrina: ${centroVetro} cm`;
 
   if (totalGlassSize > 0) {
-    document.getElementById("ingombro").innerHTML = `Spazio rimanente: ${totalGlassSize} mm`;
-    document.getElementById("gap").innerHTML = `Distanza tra pannelli: ${gap} mm`;
+    document.getElementById("ingombro").innerHTML = `Spazio rimanente: ${totalGlassSize} cm`;
+    document.getElementById("gap").innerHTML = `Distanza tra pannelli: ${gapArr} cm`;
   } else {
     document.getElementById("ingombro").innerHTML = "La misura del vetro non è sufficiente per i pannelli selezionati.";
     document.getElementById("gap").innerHTML = "";
